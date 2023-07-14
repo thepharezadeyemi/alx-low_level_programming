18baac9f3a6dadfe0e63aa47a1a16400fd8b5a25
Pharez <pharezadeyemi@gmail.com>
2023-07-14 07:42:00 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,7 @@
 
 #include <unistd.h>
 
+
 /**
  * main - Entry point
  * 
@@ -9,6 +10,6 @@
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
