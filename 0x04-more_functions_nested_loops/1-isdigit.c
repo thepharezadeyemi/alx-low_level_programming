4e9c1989c8de2d5118c2681a10d3024ea431463d
Pharez <pharezadeyemi@gmail.com>
2023-07-21 14:19:44 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,7 +5,12 @@
  * @c: int to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isdigit(int c:);
+int _isdigit(int c:)
 {
- return (c >= '0' && c <= '9');
+	
+	if(c >= '0' && c <= '9');
+	{
+	return (1);
+	}
+	return (0);
 }
