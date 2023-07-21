2110b8d08d4395c66216bbba2a3ed7813d7bdde8
Pharez <pharezadeyemi@gmail.com>
2023-07-21 16:02:31 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - checks for checks for a digit (0 through 9).
- *
  * Return: Always 0.
  */
 
@@ -11,7 +10,7 @@ void print_numbers(void)
 {
 	int c;
 
-	for (c = 48; c < 58; c++)
+	for (c = 0; c <= 9; c++)
 	{
 	_putchar(c);
 	}
