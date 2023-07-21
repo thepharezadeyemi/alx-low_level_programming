91ac29657eb9f0a81f04e33b0c10b547cd5f6fad
Pharez <pharezadeyemi@gmail.com>
2023-07-21 16:19:57 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,16 +8,17 @@
 void print_most_numbers(void)
 
 {
-	int c;
+	int c = 0;
 
-	for (c = 48; c < 58; c++)
+	for (c < =58; c++)
 	{
-	if (c != 50)
+	if (c == 2 || c == 4)
 	{
-	if (c != 52)
+	continue;
+	}
+	else
 	{
 	_putchar(c);
-	}
 	}
 	}
 	_putchar('\n');
