7b86a147cbea0bfa020fb31840e3de1b6488b1d1
Pharez <pharezadeyemi@gmail.com>
2023-07-27 16:30:34 -0700

    strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,21 +16,21 @@ char *cap_string(char *str)
 			index++;
 
 		if (str[index - 1] == ' ' ||
-		 str[index - 1] == ',' ||
-		 str[index - 1] == ';' ||
-		 str[index - 1] == '.' ||
-		 str[index - 1] == '!' ||
-		 str[index - 1] == '?' ||
-		 str[index - 1] == '"' ||
-		 str[index - 1] == '(' ||
-		 str[index - 1] == ')' ||
-		 str[index - 1] == '{' ||
-		 str[index - 1] == '}' ||
-		 index == 0)
+		str[index - 1] == ',' ||		
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
 			str[index] -= 32;
 
 		index++;
 	}
-
 	return (str);
 }
