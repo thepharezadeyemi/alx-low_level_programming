0b042caf2531aa1ee4bd3a02ce750655bd941d0d
thepharezadeyemi <pharezadeyemi@gmail.com>
2023-08-09 14:56:53 +0100

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,8 +24,8 @@ int **alloc_grid(int width, int height)
 		{
 			for (; x >= 0; x--)
 				free(mee[x]);
-				free(mee);
-				return (NULL);
+			free(mee);
+			return (NULL);
 		}
 	}
 	for (x = 0; x < height; x++)
