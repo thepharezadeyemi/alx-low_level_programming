0e46ade94681f819a293761056185b5f2588a09b
thepharezadeyemi <pharezadeyemi@gmail.com>
2023-10-12 17:36:46 +0100

    print strings

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * @separator: strings to print the between numbers
+ * @n: number of integers to print
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int j, kkk;
+
+	va_list pntr;
+
+	va_start(pntr, n);
+
+	for (j = 0; j < n; j++)
+	{
+		kkk = va_arg(pntr, const unsigned int);
+			printf("%d", kkk);
+
+		if (j != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(pntr);
+}
+
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/*
+ * print_strings - print strings passed to a function
+ * @separator: separator between strings
+ * @n: number of strings to print
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int j;
+	char *str;
+
+	va_list ptr_str;
+
+	va_start(ptr_str, n);
+
+	for (j = 0; j < n; j++)
+	{
+		str = va_arg(ptr_str, char *);
+
+		if (str == NULL)
+		{
+			printf("(Null)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+
+		if (j != (n - 1) && separator != NULL)
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr_str);
+}
